92fe82e35db17aca1fc12af753c261b57436ccfb
Sayin <luffyp2001@gmail.com>
2022-11-10 18:30:02 -0500

    Filter

--- filter.py
+++ filter.py
@@ -1,2 +1,16 @@
 from data import matches
 
+print(matches)
+
+print('/'*100)
+
+print(f'Total of matches {len(matches)}')
+
+print('/'*100)
+
+team_wins = list(filter(lambda item: item['home_team_result'] == 'Win', matches))
+print(team_wins)
+
+print('/'*100)
+
+print(f'Total of matches {len(team_wins)}')
